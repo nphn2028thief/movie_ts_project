fde7f30e5e04d7a988115cce4fe098d058fd930f
Nguyen Phan Hoai Nhan <nphn2082thief@gmail.com>
2023-05-03 12:24:31 +0700

    test error case part 4 and re-commit make media category section part 2

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -41,7 +41,7 @@ export default function HomePage() {
           mediaCategory={tmdbConfigs.mediaCategory.popular}
         />
 
-        {/* <Box>
+        <Box>
           {mediaSlides.map((mediaSlide) => (
             <Container key={mediaSlide.id} title={mediaSlide.title}>
               <MediaSlide
@@ -50,7 +50,7 @@ export default function HomePage() {
               />
             </Container>
           ))}
-        </Box> */}
+        </Box>
       </Stack>
     </Wrapper>
   );
